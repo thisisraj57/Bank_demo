c8dd3b6d011f150fbf2820ff5a3522acc96d43d0
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-25 15:56:24 +0530

    Add new code generated at 2024-06-25T15:56:23.665712

---
+++ ab/BookService.java
@@ -0,0 +1,25 @@
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
